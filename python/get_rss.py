f6773b38117f18d7f5efbe1075252b9c3906b0aa
yurixxx8 <misaki-suzuki@gaji.jp>
2018-01-08 16:05:30 +0900

    記事の投稿日時を取得

--- python/get_rss.py
+++ python/get_rss.py
@@ -1,4 +1,5 @@
 import feedparser
+import datetime
 
 # RSSのURL
 feed_url = 'https://www.nxworld.net/feed/'
@@ -8,6 +9,15 @@ feed_result = feedparser.parse(feed_url)
 
 for entry in feed_result.entries:
   title = entry.title
+  # time.struct_time型のオブジェクトが生成される
+  published = entry.published_parsed
+  # tuple型をunpack
+  # date_time datetime型
+  date_time = datetime.datetime(*published[:6])
+  # datetime型からstringに変換 2017/11/29
+  date_str = datetime.datetime.strftime(date_time, "%Y/%m/%d")
   link = entry.link
+
   print(title)
+  print(date_str)
   print(link)
